8dc711b9026071060c8d74d4c77ece05bb74ed00
Logesh-Pro <logeshb451@gmail.com>
2025-12-22 23:38:22 +0530

    Esscentials

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,10 @@
 console.log("Hello, World!");
+// "Everything in javascript happens inside an execution context"
+// big box first part memory component or (heavy word) variable enviroment
+// second part code component or (heavy word) thread of execution
+// every code exicuted one line at a time
+// JavaScript is synchronous single threaded language
+// single threaded means one command at a time
+// sysnchronous means one at a time in order
+// memory component is where variables and functions are stored
+// code component is where the whole javascript code is executed
