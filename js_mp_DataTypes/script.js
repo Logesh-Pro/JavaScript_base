e6cf79ca95d12ac199f3537dee164df8b8043f53
Logesh-Pro <logeshb451@gmail.com>
2025-12-25 01:03:22 +0530

    project_2

---
+++ js_mp_DataTypes/script.js
@@ -0,0 +1,31 @@
+console.log("all primitive data type in javascript");
+// 1. String
+let name = 'Logesh';
+// 2. Number
+let age = 18;
+// 3. Boolean
+let isStudent = true;
+// 4. Undefined
+let address;
+// 5. Null
+let intenshipstatus = null;
+// 6. bigint
+let universityId = 1234567890123456789012345678901234567890n;
+// 7. Symbol
+let universityStudentId = Symbol("studentId");
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Address:", address);
+console.log("Internship Status:", intenshipstatus);
+console.log("University ID:", universityId);
+console.log("University Student ID:", universityStudentId);
+
+console.log("type of name:", typeof name);
+console.log("type of age:", typeof age);
+console.log("type of isStudent:", typeof isStudent);
+console.log("type of address:", typeof address);
+console.log("type of intenshipstatus:", typeof intenshipstatus);
+console.log("type of universityId:", typeof universityId);
+console.log("type of universityStudentId:", typeof universityStudentId);
