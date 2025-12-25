fdd47d3c2e14c0f44a3b3f5663b9a2a55976b4dc
Logesh-Pro <logeshb451@gmail.com>
2025-12-25 19:34:06 +0530

    project 4

---
+++ loops_in_js/script.js
@@ -0,0 +1,14 @@
+console.log("loops in js");
+let sum=0;
+for(let i=0;i<5;i++){
+    sum += 10;
+}
+i=0;
+while(i<10){
+    sum += 10;
+    i++;
+}
+do{
+    console.log("The sum is: " + sum);
+}
+while(0>sum);
